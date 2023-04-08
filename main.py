efb60dce22c18a64c85f4adc0841b7fc8e8975dc
Master_01 <meleuz218@yandex.ru>
2023-04-08 21:22:00 +0500

    Proba исправление

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-# url = "https://health-diet.ru/base_of_food/food_24507/"
-# response = requests.get(url)
-# # print(response)
-# # print(dir(response))
-# html_txt = response.text
-# print(html_txt)
-# # Сохраняем код
-# # создать и открыть файл для записи кода HTML
-# with open("index.html", "w") as file:
-#     file.write(html_txt)
+url = "https://health-diet.ru/base_of_food/food_24507/"
+response = requests.get(url)
+# print(response)
+# print(dir(response))
+html_txt = response.text
+print(html_txt)
+# Сохраняем код
+# создать и открыть файл для записи кода HTML
+with open("index.html", "w") as file:
+    file.write(html_txt)
 
 with open("index.html") as file:
     html_txt = file.read()
